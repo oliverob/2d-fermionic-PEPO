549a6e1e7c6e5af60b32a1329bc2d147bec14202
Oliver O'Brien <oliverobrien111@gmail.com>
2024-02-15 16:35:04 +0000

    Got electron app working

--- arbitary_graph_operator_conversion.jl
+++ arbitary_graph_operator_conversion.jl
@@ -1,7 +1,11 @@
+module PEPOAnalysis
+    
 using Graphs
 using MetaGraphsNext
 using GraphPlot, Compose
 using QuantumAlgebra
+
+export convert_to_PEPO, testing_odd_algebra, testing_even_algebra, display_graphs
 
 function convert_to_PEPO(G, defect=false)
     new_graph = MetaGraph(DiGraph(), label_type=Int, vertex_data_type=Tuple{Symbol, Int}, edge_data_type=Int)
@@ -38,26 +42,24 @@ function has_order_higher_than_path(PEPO,source,path, leg)
     return PEPO[leg_edge...] > PEPO[path_edge...]
 end
 
-function fermionic_XX(PEPO,start, target)
-    shortest_path = a_star(G, start, target)
+function fermionic_XX(PEPO, start, target)
+    shortest_path = a_star(Graph(PEPO), start, target)
 
     operators = σz()^2
     for edge in shortest_path
-        source = src(edge) # Vertex tensor
-        destination = dst(edge) # Vertex tensor
-        src_neighbours = all_neighbors(PEPO, src(edge))
-        dest_neighbours = all_neighbors(PEPO, dst(edge))
-        path = intersect(src_neighbours, dest_neighbours)[1] # GHZ tensor
-        z_edges_src = filter((x)-> has_order_higher_than_path(PEPO,source,path, x),src_neighbours)
-        z_edges_dst = filter((x)-> has_order_higher_than_path(PEPO,destination,path, x),dest_neighbours)
-        if PEPO[path][1] == :d
-            operators *= σz(path)
-            break;
+        source = src(edge) 
+        destination = dst(edge) 
+        vertex_tensor = PEPO[source][1] == :v ? source : destination
+        ghz_tensor = PEPO[source][1] == :v ? destination : source
+        vertex_neighbours = all_neighbors(PEPO, vertex_tensor)
+        z_edges_vertex = filter((x)-> has_order_higher_than_path(PEPO,vertex_tensor,ghz_tensor, x),vertex_neighbours)
+        operators *= prod(map((x)-> σz(x),z_edges_vertex))
+
+        if destination == ghz_tensor
+            operators *= has_edge(PEPO,vertex_tensor, ghz_tensor) ? 1 : -1
+            operators *= σy(ghz_tensor)
         end
-        operators *= has_edge(PEPO,source, path) ? 1 : -1
-        operators *= prod(map((x)-> σz(x),z_edges_src))
-        operators *= prod(map((x)-> σz(x),z_edges_dst))
-        operators *= σy(path)
+
     end
     return normal_form(operators)
 end
@@ -111,7 +113,7 @@ function testing_even_algebra(PEPO, interaction_graph)
     total_hopping_terms = 0
     for current_edge in edges(interaction_graph)
         i, j = src(current_edge), dst(current_edge)
-        current_hopping_term = fermionic_XX(PEPO,i, j) 
+        current_hopping_term = fermionic_XX(PEPO, i, j) 
         total_pauli_weight += length(collect(keys(current_hopping_term.terms))[1].bares.v)
         total_hopping_terms += 1
         for other_edge in edges(interaction_graph)
@@ -171,36 +173,17 @@ function testing_odd_algebra(PEPO, interaction_graph)
 end
 
 function display_graphs(PEPO, G, interaction_graph)
-    display(gplot(G))
+    # display(gplot(G))
+    println("Interaction Graph")
     display(gplot(interaction_graph))
 
-    display(gplot(PEPO, edgelabel=map((x)-> x[2],sort(collect(PEPO.edge_data), by=x->x[1])), edgelabelc="white", EDGELABELSIZE=8, nodelabel=map((x)-> x[2][2][1],sort(collect(PEPO.vertex_properties), by=x->x[1]))))
+    println("PEPO")
+    display(gplot(PEPO, edgelabel=map((x)-> x[2],sort(collect(PEPO.edge_data), by=x->x[1])), edgelabelc="blue", EDGELABELSIZE=8, nodelabel=map((x)-> x[2][2][1],sort(collect(PEPO.vertex_properties), by=x->x[1]))))
     
-    display(gplot(PEPO, edgelabel=map((x)-> x[2],sort(collect(PEPO.edge_data), by=x->x[1])), edgelabelc="white", EDGELABELSIZE=8, nodelabel=map((x)-> x[2][1],sort(collect(PEPO.vertex_properties), by=x->x[1]))))
+    # display(gplot(PEPO, edgelabel=map((x)-> x[2],sort(collect(PEPO.edge_data), by=x->x[1])), edgelabelc="white", EDGELABELSIZE=8, nodelabel=map((x)-> x[2][1],sort(collect(PEPO.vertex_properties), by=x->x[1]))))
 end
 
-interaction_graph = grid([7,7])
-# G = smallgraph(:petersen)
-G = interaction_graph
-# G = Graph(prim_mst(interaction_graph))
-
-PEPO = convert_to_PEPO(G, false)
-
-if (!is_connected(G))
-    throw("Not connected")
 end
 
-display_graphs(PEPO, G, interaction_graph)
 
-try 
-    println("Odd algebra average Pauli weight: ", testing_odd_algebra(PEPO, interaction_graph))
-catch (e)
-    println("FAILED ODD ALGEBRA: $e")
-end
-
-try 
-    println("Even algebra average Pauli weight: ", testing_even_algebra(PEPO, interaction_graph))
-catch (e)
-    println("FAILED EVEN ALGEBRA: $e")
-end
 
